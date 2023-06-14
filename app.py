9ca1f16e6cbcaad8bacc0c680f41c9d37b404a2b
Ezeikpe <111017925+Ezeikpe@users.noreply.github.com>
2023-06-13 17:53:53 -0700

    Main App File

--- app.py
+++ app.py
@@ -1,74 +1,28 @@
-#importing packages 
-from  tkinter import * 
-import tkinter.messagebox
-#function to enter the task in the Listbox
-def entertask():
-    #A new window to pop up to take input
-    input_text=""
-    def add():
-        input_text=entry_task.get(1.0, "end-1c")
-        if input_text=="":
-            tkinter.messagebox.showwarning(title="Warning!",message="Please Enter some Text")
-        else:
-            listbox_task.insert(END,input_text)
-            #close the root1 window
-            root1.destroy()
-    root1=Tk()
-    root1.title("Add task")
-    entry_task=Text(root1,width=40,height=4)
-    entry_task.pack()
-    button_temp=Button(root1,text="Add task",command=add)
-    button_temp.pack()
-    root1.mainloop()
-    
+from flask import Flask, render_template, request, redirect, url_for
 
-#function to facilitate the delete task from the Listbox
-def deletetask():
-    #selects the slected item and then deletes it 
-    selected=listbox_task.curselection()
-    listbox_task.delete(selected[0])
-#Executes this to mark completed 
+app = Flask(__name__)
 
-def markcompleted():
-    marked=listbox_task.curselection()
-    temp=marked[0]
-    #store the text of selected item in a string
-    temp_marked=listbox_task.get(marked)
-    #update it 
-    temp_marked=temp_marked+" ✔"
-    #delete it then insert it 
-    listbox_task.delete(temp)
-    listbox_task.insert(temp,temp_marked)
-#creating the initial window
-window=Tk()
-#giving a title
-window.title("Schedular - a To-Do List Solution")
+# initialize an empty list to store our to-do items
+todo_items = []
 
+@app.route('/')
+def index():
+    """Render the index page with the list of to-do items."""
+    return render_template('index.html', todo_items=todo_items)
 
-#Frame widget to hold the listbox and the scrollbar
-frame_task=Frame(window)
-frame_task.pack()
+@app.route('/add', methods=['POST'])
+def add():
+    """Add a new to-do item to the list."""
+    item = request.form['item']
+    todo_items.append(item)
+    return redirect(url_for('index'))
 
-#to hold items in a listbox
-listbox_task=Listbox(frame_task,bg="black",fg="white",height=15,width=50,font = "Helvetica")  
-listbox_task.pack(side=tkinter.LEFT)
+@app.route('/delete/<int:index>')
+def delete(index):
+    """Delete a to-do item from the list."""
+    del todo_items[index]
+    return redirect(url_for('index'))
 
-#Scrolldown in case the total list exceeds the size of the given window 
-scrollbar_task=Scrollbar(frame_task)
-scrollbar_task.pack(side=tkinter.RIGHT,fill=tkinter.Y)
-listbox_task.config(yscrollcommand=scrollbar_task.set)
-scrollbar_task.config(command=listbox_task.yview)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-
-#Button widget 
-entry_button=Button(window,text="Add task",width=50,command=entertask)
-entry_button.pack(pady=3)
-
-delete_button=Button(window,text="Delete selected task",width=50,command=deletetask)
-delete_button.pack(pady=3)
-
-mark_button=Button(window,text="Mark as completed ",width=50,command=markcompleted)
-mark_button.pack(pady=3)
-
-
-window.mainloop()
