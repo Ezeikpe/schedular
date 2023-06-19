97f1b31e866089ac5cbdb667e0eeaf38836ca93f
Ezeikpe <111017925+Ezeikpe@users.noreply.github.com>
2023-06-18 17:13:20 -0700

    Update main.py

--- virtualenv/main.py
+++ virtualenv/main.py
@@ -1,28 +1,39 @@
-python
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# initialize an empty list to store our to-do items
+# Define a list to store the to-do items
 todo_items = []
 
-@app.route('/')
-def index():
-    """Render the index page with the list of to-do items."""
-    return render_template('index.html', todo_items=todo_items)
+# Define a route to handle the home page
+@app.route("/")
+def home():
+    return render_template("index.html", todo_items=todo_items)
 
-@app.route('/add', methods=['POST'])
+# Define a route to handle adding a new item
+@app.route("/add", methods=["POST"])
 def add():
-    """Add a new to-do item to the list."""
-    item = request.form['item']
-    todo_items.append(item)
-    return redirect(url_for('index'))
-  
-@app.route('/delete/<int:index>')
-def delete(index):
-    """Delete a to-do item from the list."""
-    del todo_items[index]
-    return redirect(url_for('index'))
+    # Get the text from the form
+    item_text = request.form["item_text"]
 
-if __name__ == '__main__':
+    # Add the new item to the list
+    todo_items.append({"text": item_text, "completed": False})
+
+    # Redirect back to the home page
+    return redirect(url_for("home"))
+
+# Define a route to handle removing completed items
+@app.route("/remove", methods=["POST"])
+def remove():
+    # Get the indexes of the completed items
+    completed_indexes = [int(index) for index in request.form.getlist("item_completed")]
+
+    # Remove the completed items from the list
+    global todo_items
+    todo_items = [item for index, item in enumerate(todo_items) if index not in completed_indexes]
+
+    # Redirect back to the home page
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
     app.run(debug=True)
